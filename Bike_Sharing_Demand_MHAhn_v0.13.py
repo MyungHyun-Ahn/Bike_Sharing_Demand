1df69dab004e8eb662b6aaaf994eb18cf0048597
Myunghyun_Ahn <ahn020367@gmail.com>
2022-03-31 13:00:21 +0900

    그래프 png 파일 / windspeed 값 예측

--- Bike_Sharing_Demand_MHAhn_v0.13.py
+++ Bike_Sharing_Demand_MHAhn_v0.13.py
@@ -103,6 +103,7 @@ vif_n['feateres'] = df_n[X_col].columns
 vif_n
 
 '''
+   정규화 수행 전
    VIF Factor feateres
 0   86.558560   season
 1  244.435192     temp
@@ -111,6 +112,7 @@ vif_n
 '''
 
 '''
+   정규화 수행 후
    VIF Factor feateres
 0   86.062461   season
 1  224.015624     temp
@@ -155,7 +157,7 @@ for name in one_hot_data:
    df_n = pd.get_dummies(df_n, columns=[name])
 
 
-# 정규 분포 그리기
+# 분포 그래프 그리기
 f, ax = plt.subplots(nrows=3, figsize=(10,8))
 sns.distplot(df_n['count'], ax=ax[0])
 sns.distplot(df_n['casual'], ax=ax[1])
@@ -197,18 +199,30 @@ print('왜도: {}, 첨도: {}'.format(df_n['log_registered'].skew(), df_n['log_r
 from scipy import stats
 
 f = plt.figure(figsize=(10, 10))
-f.add_subplot(3, 2, 1)
+ax1 = f.add_subplot(3, 2, 1)
 res = stats.probplot(df_n['count'], plot=plt)
-f.add_subplot(3, 2, 2)
+ax1.set_title('count probability')
+
+ax2 = f.add_subplot(3, 2, 2)
 res = stats.probplot(df_n['log_count'], plot=plt)
-f.add_subplot(3, 2, 3)
+ax2.set_title('log_count probability')
+
+ax3 = f.add_subplot(3, 2, 3)
 res = stats.probplot(df_n['casual'], plot=plt)
-f.add_subplot(3, 2, 4)
+ax3.set_title('casual probability')
+
+ax4 = f.add_subplot(3, 2, 4)
 res = stats.probplot(df_n['log_casual'], plot=plt)
-f.add_subplot(3, 2, 5)
+ax4.set_title('log_casual probability')
+
+ax5 = f.add_subplot(3, 2, 5)
 res = stats.probplot(df_n['registered'], plot=plt)
-f.add_subplot(3, 2, 6)
+ax5.set_title('registered probability')
+
+ax6 = f.add_subplot(3, 2, 6)
 res = stats.probplot(df_n['log_registered'], plot=plt)
+ax6.set_title('log_registered probability')
+
 plt.tight_layout()
 plt.show()
 
@@ -218,3 +232,108 @@ df_n.columns
 print('왜도: {}, 첨도: {}'.format(df_n['temp'].skew(), df_n['temp'].kurt()))  # 왜도: 0.003690844422470916, 첨도: -0.9145302637630794
 print('왜도: {}, 첨도: {}'.format(df_n['humidity'].skew(), df_n['humidity'].kurt()))  # 왜도: -0.08633518364548581, 첨도: -0.7598175375208864
 print('왜도: {}, 첨도: {}'.format(df_n['windspeed'].skew(), df_n['windspeed'].kurt()))  # 왜도: 0.5887665265853944, 첨도: 0.6301328693364932
+
+f = plt.figure(figsize=(10, 10))
+ax1 = f.add_subplot(3, 1, 1)
+res = stats.probplot(df_n['temp'], plot=plt)
+ax1.set_title('temp probability')
+
+ax2 = f.add_subplot(3, 1, 2)
+res = stats.probplot(df_n['humidity'], plot=plt)
+ax1.set_title('humidity probability')
+
+ax3 = f.add_subplot(3, 1, 3)
+res = stats.probplot(df_n['windspeed'], plot=plt)
+ax1.set_title('windspeed probability')
+
+plt.tight_layout()
+plt.show()
+
+
+# 습도 데이터에 의문점이 생겨 데이터를 찾던중 0값이 연속적이지 못함을 발견
+# 데이터 엑셀 파일을 살펴본 결과 100값은 자연스러움 >> 연속적
+# 하루치 데이터가 빠져있음
+
+df_n[df_n['humidity'] == 0] # 22개 > 하루치 데이터
+
+df_n.columns
+
+df_n[df_n['windspeed']==0] # 1313개 > 무수히 많음
+
+# 풍속 먼저 예측
+
+# 풍속 예측 데이터 준비
+# 풍속은 날씨 관련 데이터 이므로 날씨와 관련된 데이터를 독립변수 X로 설정
+# ['temp', 'humidity', 'hour', 'season_1', 'season_2', 'season_3', 'season_4', 'weather_1', 'weather_2', 'weather_3', 'weather_4']
+# 다만 습도에 0값 데이터는 신뢰도가 떨어지므로 삭제
+
+
+# 훈련 데이터 준비
+wind_x = ['temp', 'windspeed', 'humidity', 'hour', 'season_1', 'season_2', 'season_3', 'season_4', 'weather_1', 'weather_2', 'weather_3', 'weather_4']
+
+wind_df = df_n[wind_x]
+del_hu = wind_df[wind_df['humidity'] == 0]
+wind_df.drop(del_hu.index, axis=0, inplace=True)
+wind_test = wind_df[wind_df['windspeed'] == 0]
+wind_df.drop(wind_test.index, axis=0, inplace=True)
+wind_dfX = wind_df.drop('windspeed', axis=1)
+wind_dfy = pd.DataFrame(wind_df['windspeed'], columns=['windspeed'])
+wind_testX = wind_test.drop('windspeed', axis=1)
+
+# 그리드 서치를 위한 임포트
+from sklearn.model_selection import GridSearchCV
+
+from sklearn.ensemble import GradientBoostingRegressor
+# 그라디언트 부스트 파라미터
+GBR_param = [{
+   'n_estimators': [10, 100, 1000, 2000],
+   'max_features': range(1,4),
+   'max_depth': range(3,5),
+   'learning_rate': np.linspace(0.1, 1, 10)
+}]
+
+GBR_model = GradientBoostingRegressor()
+
+GBR_gcv = GridSearchCV(GBR_model, param_grid=GBR_param, scoring='r2', cv=5, n_jobs=-1)
+GBR_gcv.fit(wind_dfX, wind_dfy.values.ravel())
+
+print('베스트 하이퍼 파라미터: {0}'.format(GBR_gcv.best_params_))
+print('베스트 하이퍼 파라미터 일 때 정확도: {0:.2f}'.format(GBR_gcv.best_score_))
+# 베스트 하이퍼 파라미터: {'learning_rate': 0.1, 'max_depth': 3, 'max_features': 2, 'n_estimators': 100}
+# 베스트 하이퍼 파라미터 일 때 정확도: 0.12
+
+best_model = GradientBoostingRegressor(learning_rate=0.1, max_depth=3, max_features=2, n_estimators=100)
+best_model.fit(wind_dfX, wind_dfy)
+
+wind_pre_y = best_model.predict(wind_testX)
+
+wind_pre_y
+
+len(wind_pre_y)
+len(df_n[df_n['windspeed']==0])
+
+wind_zeros = df_n[df_n['windspeed']==0]
+
+wzeros_index = list(wind_zeros.index)
+
+for i in range(len(wind_pre_y)):
+   df_n.loc[wzeros_index[i], 'windspeed'] = wind_pre_y[i]
+
+# 풍속 0 값 처리 완료
+df_n.head(10)
+
+# windspeed 데이터가 연속적으로 채워짐
+
+'''
+             datetime      temp  humidity  windspeed  casual  registered  count  year  ...  dayofweek_2  dayofweek_3  dayofweek_4  dayofweek_5  dayofweek_6  log_count  log_casual  log_registered
+0 2011-01-01 00:00:00  0.224490        81  10.517576       3          13     16  2011  ...            0            0            0            1            0   2.833213    1.386294        2.639057
+1 2011-01-01 01:00:00  0.204082        80  10.698296       8          32     40  2011  ...            0            0            0            1            0   3.713572    2.197225        3.496508
+2 2011-01-01 02:00:00  0.204082        80  10.823663       5          27     32  2011  ...            0            0            0            1            0   3.496508    1.791759        3.332205
+3 2011-01-01 03:00:00  0.224490        75  11.855815       3          10     13  2011  ...            0            0            0            1            0   2.639057    1.386294        2.397895
+4 2011-01-01 04:00:00  0.224490        75  11.855815       0           1      1  2011  ...            0            0            0            1            0   0.693147    0.000000        0.693147
+5 2011-01-01 05:00:00  0.224490        75   6.003200       0           1      1  2011  ...            0            0            0            1            0   0.693147    0.000000        0.693147
+6 2011-01-01 06:00:00  0.204082        80  11.207899       2           0      2  2011  ...            0            0            0            1            0   1.098612    1.098612        0.000000
+7 2011-01-01 07:00:00  0.183673        86  11.253412       1           2      3  2011  ...            0            0            0            1            0   1.386294    0.693147        1.098612
+8 2011-01-01 08:00:00  0.224490        75  12.903327       1           7      8  2011  ...            0            0            0            1            0   2.197225    0.693147        2.079442
+9 2011-01-01 09:00:00  0.306122        76  14.309837       8           6     14  2011  ...            0            0            0            1            0   2.708050    2.197225        1.945910
+'''
